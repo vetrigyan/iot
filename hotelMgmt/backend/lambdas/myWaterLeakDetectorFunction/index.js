a30cc3e3a73618b101f44b5d774c1f487cfa10d0
EC2 Default User <ec2-user@ip-172-31-22-181.ec2.internal>
2019-10-31 18:36:52 +0000

    initial commit

--- hotelMgmt/backend/lambdas/myWaterLeakDetectorFunction/index.js
+++ hotelMgmt/backend/lambdas/myWaterLeakDetectorFunction/index.js
@@ -30,65 +30,36 @@ const client = new AWSAppSyncClient({
     disableOffline: true      //Uncomment for AWS Lambda
 });
 
-    const updateListing = gql `mutation UpdateListing($input: UpdateListingInput!) {
-  updateListing(input: $input) {
+const createAlert = gql `mutation CreateAlert($input: CreateAlertInput!) {
+  createAlert(input: $input) {
     id
+    sourceObject
+    type
+    severity
     title
-    company
-    url
-    status
-    favorite
-    notes
-    relatedDate
-    contactName
-    contactEmail
-    contactPhoneNumber
-    createdAt
-    updatedAt
   }
-}`;
+}
+`;
 
   const vari = {
-              id: "018a367e-f6d9-4387-8266-8ffc398548cb",
-              notes: "Pile up  of Notes!"
+              id: "somedevice",
+              sourceObject: "somedevice",
+              type: "WATER_LEAK",
+              severity: "MAJOR",
+              title: "Serious issue in Room 101"  
   };
 
 exports.handler = (event, context, callback) => {
         console.log('Going to invoke mutate with vari =  -----------> ' + vari);
 client.hydrated().then(function (client) {
-    //Now run a query
-    //client.query({ query: query })
-    //client.query({ query: query, fetchPolicy: 'network-only' })   //Uncomment for AWS Lambda
-     //   .then(function logData(data) {
-      //      console.log('results of query: ', data);
-       // })
-       // .catch(console.error);
-    client.mutate({ mutation: updateListing, variables:{input: vari} })
+    client.mutate({ mutation: createAlert, variables:{input: vari} })
     .then(function logData(data) {
-        console.log('(Mutate): Updated Listing Data ----------->', data);
+        console.log('(Mutate): Created Alert Data ----------->', data);
     })
     .catch(console.error);
 
 });
     
-    /*await client.mutate({
-      updateListing,
-      variables: {
-        input: {
-              id: "018a367e-f6d9-4387-8266-8ffc398548cb",
-              notes: "Nothing at all!"
-        }
-      }
-    });*/
-    /*const mutation = gql`mutation Test($value: String!) {
-          test(value: $value)
-        }`;
-    await graphqlClient.mutate({
-      mutation,
-      variables: {
-        value: 'test'
-      }
-    });*/
         const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!')
