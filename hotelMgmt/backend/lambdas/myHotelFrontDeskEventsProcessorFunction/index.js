113d670fe55475330d1327f66c4e314380f7004f
EC2 Default User <ec2-user@ip-172-31-22-181.ec2.internal>
2019-11-01 08:38:11 +0000

    initial commit

--- hotelMgmt/backend/lambdas/myHotelFrontDeskEventsProcessorFunction/index.js
+++ hotelMgmt/backend/lambdas/myHotelFrontDeskEventsProcessorFunction/index.js
@@ -1,8 +1,11 @@
 
 "use strict";
+console.log('starting function');
+
 
 // If you want to use AWS...
 const AWS = require('aws-sdk');
+var docClient = new AWS.DynamoDB.DocumentClient({region : 'us-east-1'});
 const credentials = AWS.config.credentials;
 
 global.WebSocket = require('ws');
@@ -30,39 +33,82 @@ const client = new AWSAppSyncClient({
     disableOffline: true      //Uncomment for AWS Lambda
 });
 
-const createAlert = gql `mutation CreateAlert($input: CreateAlertInput!) {
-  createAlert(input: $input) {
+const updateSensorRoomData = gql `mutation UpdateSensorRoomData($input: UpdateSensorRoomDataInput!) {
+  updateSensorRoomData(input: $input) {
     id
-    sourceObject
-    type
-    severity
-    title
-    notes
-    createdAt
-    updatedAt
+    occupied
   }
 }
 `;
-const vari = {
- sourceObject: "somedevice"
- type: "WATER_LEAK"
- severity: "MAJOR"
- title: "Serious issue in Room"  
-};
-exports.handler = (event, context, callback) => {
-        console.log('Going to invoke create mutate with  -----------> ');
+
+
+let updateRoomStatusForSensors = function(sensorId, roomId, roomCheckInStatus) {
+    var isOccupied = roomCheckInStatus == 1 ? true : false;
+  const updateVari = {
+              id: sensorId,
+              occupied: isOccupied
+  };
+        console.log('Going to invoke update mutate with vari =  -----------> ' + updateVari);
 client.hydrated().then(function (client) {
-    client.mutate({ mutation: createAlert, variables: {input: vari}} })
+    client.mutate({ mutation: updateSensorRoomData, variables:{input: updateVari} })
     .then(function logData(data) {
-        console.log('(Mutate): Create Alert Data ----------->', data);
+        console.log('(Mutate): Updated SensorRoomData ----------->', data);
     })
     .catch(console.error);
 
 });
+}
     
-        const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!')
-    };
-    return response;
-};
+exports.handler = (event, context, callback) => {
+  event.Records.forEach(record => {
+    const { body } = record;
+    console.log('Body is = ' + body);
+    if (body == null) {
+      console.log("Empty event received.")
+      return {};
+    }
+    var r = JSON.parse(body);
+        r.Records.forEach(function(rec){
+            console.log("Inside for loop");
+        var roomId1 = parseInt(rec.body.roomOccupancyStatus.roomId);
+        var roomCheckInStatus = parseInt(rec.body.roomOccupancyStatus.roomCheckInStatus);
+        console.log('Inside for loop... roomId1' + roomId1);
+        var tableName = "SensorRoomData-a6vu6lziizbflprigc7ncn7ta4-hotelmgmt";
+    
+        var params = {
+            TableName: tableName,
+            IndexName: "roomId-index-copy",
+            KeyConditionExpression: "roomId = :v_title",
+            ExpressionAttributeValues: { ':v_title': roomId1},
+            ProjectionExpression: "id",
+            ScanIndexForward: false
+        };
+        
+        var putObjectPromise = docClient.query(params).promise();
+        putObjectPromise.then(function(data) {
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                var alertExists = false;
+                if (data.Items == null) {
+                    console.log('No sensor with roomId=' + roomId1 + ' exists');
+                } else {
+                    console.log("Sensor with roomId exists... will need to iterate and update each TBD");
+                    // iterate over sensor records to update the occupied status
+                    data.Items.forEach(function(item) {
+                      console.log("Inside items loop");
+                      var sensorId = item.id;
+                      console.log('Invoking updateRoomStatusForSensors...' + sensorId +', ' + roomId1 + 
+                      ', ' + roomCheckInStatus);
+                      updateRoomStatusForSensors(sensorId, roomId1, roomCheckInStatus);
+                    });
+                    
+                }
+        }).catch(function(err) {
+                    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                  console.log(err);
+                });
+        
+      //return {};
+    });
+    });
+    callback(null, `Successfully processed ${event.Records.length} records.`);
+}
--- hotelMgmt/backend/lambdas/myWaterLeakDetectorFunction/index.js
+++ hotelMgmt/backend/lambdas/myWaterLeakDetectorFunction/index.js
@@ -19,6 +19,11 @@ const type = AUTH_TYPE.AWS_IAM;
 // Import gql helper
 const gql = require('graphql-tag');
 
+var sns = new AWS.SNS();
+
+var FLOW_THRESHOLD = 50;
+const ALARM_TYPE_WATERLEAK = "WATER_LEAK";
+
 // Set up Apollo client
 const client = new AWSAppSyncClient({
     url: process.env.GRAPHQL_API_URL,
@@ -30,7 +35,7 @@ const client = new AWSAppSyncClient({
     disableOffline: true      //Uncomment for AWS Lambda
 });
 
-const createAlert = gql `mutation CreateAlert($input: CreateAlertInput!) {
+const createAlert = gql`mutation CreateAlert($input: CreateAlertInput!) {
   createAlert(input: $input) {
     id
     sourceObject
@@ -41,28 +46,215 @@ const createAlert = gql `mutation CreateAlert($input: CreateAlertInput!) {
 }
 `;
 
-  const vari = {
-              id: "somedevice",
-              sourceObject: "somedevice",
-              type: "WATER_LEAK",
-              severity: "MAJOR",
-              title: "Serious issue in Room 101"  
-  };
+const deleteAlert = gql `mutation DeleteAlert($input: DeleteAlertInput!) {
+    deleteAlert(input: $input) {
+      id
+    }
+  }
+  `;
+
+let isFlowing = function (newFlowrate) {
+    console.log('typeof Newflowrate is' + newFlowrate);
+    //var flow = parseInt(newFlowrate);
+    //var flowRate = Number(parseInt(newFlowrate));
+    //console.log('Flowrate is' + flow + ', ' + typeof(flow));
+    if (50 < newFlowrate) {
+        console.log("There IS flow");
+        return true;
+    }
+
+    console.log("There is NO flow");
+    return false;
+
+}
+
+let hasAlerted = function (sensorId, roomId, alarmType) {
+    // read dynamodb Alerts table to find out
+    var docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+
+    var table = "Alarms";
+
+    var params = {
+        TableName: table,
+        Key: {
+            "sensorId": sensorId
+        }
+    };
+
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+
+            if (data.Item.sensorId == null) {
+                console.log("No alarm exists");
+                return false;
+            } else {
+                console.log("Alarm exists");
+                return true;
+            }
+        }
+    });
+
+}
+
+let createAlarm = function (sensorId, roomId, alarmType) {
+
+    console.log("Adding a new item...");
+
+    const vari = {
+        id: sensorId,
+        sourceObject: sensorId,
+        type: alarmType,
+        severity: "CRITICAL",
+        title: "Serious issue in Room number: " + roomId,
+        notes: "Needs immediate action from personnel!!!"
+    };
+
+    console.log('Going to invoke mutate with vari =  -----------> ' + vari);
+    client.hydrated().then(function (client) {
+        client.mutate({ mutation: createAlert, variables: { input: vari } })
+            .then(function logData(data) {
+                console.log('(Mutate): Created Alert Data ----------->', data);
+            })
+            .catch(console.error);
+
+    });
+
+}
+
+let deleteAlarm = function (sensorId, roomId, alarmType) {
+
+    console.log("Attempting a conditional delete...");
+    const vari = {
+        id: sensorId
+    };
+
+    console.log('Going to invoke delete mutation with vari =  -----------> ' + vari);
+    client.hydrated().then(function (client) {
+        client.mutate({ mutation: deleteAlert, variables: { input: vari } })
+            .then(function logData(data) {
+                console.log('(Mutate): Deleted Alert  ----------->', data);
+            })
+            .catch(console.error);
+
+    });
+
+}
+
+let postAlertMessage = function (sensorId, roomId, alarmType, isRaised) {
+    var subject = "";
+    var message = "";
+    if (isRaised) {
+        subject = 'Critical Issue at Hotel Aria room number ' + roomId;
+        if (alarmType == ALARM_TYPE_WATERLEAK) {
+            message = '\n\nWater leak detected';
+        } else {
+            message = '\n\nSome issue detected';
+        }
+    } else {
+        subject = 'Issue Resolved at Hotel Aria room number ' + roomId;
+        if (alarmType == ALARM_TYPE_WATERLEAK) {
+            message = '\n\nWater leak has stopped';
+        } else {
+            message = '\n\nIssue is resolved';
+        }
+    }
+
+    var params = {
+        Subject: subject,
+        Message: message,
+        TopicArn: 'arn:aws:sns:us-east-1:206982232731:MoistureSensorTopic'
+    };
+    sns.publish(params, function (err, data) {
+        if (err) {
+            console.error("Unable to send message. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Results from sending message: ", JSON.stringify(data, null, 2));
+        }
+    });
+}
+
 
 exports.handler = (event, context, callback) => {
-        console.log('Going to invoke mutate with vari =  -----------> ' + vari);
-client.hydrated().then(function (client) {
-    client.mutate({ mutation: createAlert, variables:{input: vari} })
-    .then(function logData(data) {
-        console.log('(Mutate): Created Alert Data ----------->', data);
-    })
-    .catch(console.error);
 
-});
-    
-        const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!')
-    };
-    return response;
+    event.Records.forEach((record) => {
+        console.log('==========Stream record: ', JSON.stringify(record, null, 2));
+
+        if (record.eventName == 'INSERT') {
+            // var who = JSON.stringify(record.dynamodb.NewImage.Username.S);
+            // var when = JSON.stringify(record.dynamodb.NewImage.Timestamp.S);
+            // var what = JSON.stringify(record.dynamodb.NewImage.Message.S);
+            // var params = {
+            //     Subject: 'A new bark from ' + who, 
+            //     Message: 'Woofer user ' + who + ' barked the following at ' + when + ':\n\n ' + what,
+            //     TopicArn: 'arn:aws:sns:region:accountID:wooferTopic'
+            // };
+            // sns.publish(params, function(err, data) {
+            //     if (err) {
+            //         console.error("Unable to send message. Error JSON:", JSON.stringify(err, null, 2));
+            //     } else {
+            //         console.log("Results from sending message: ", JSON.stringify(data, null, 2));
+            //     }
+            // });
+        } else if (record.eventName == 'MODIFY') {
+            var sensorId = JSON.stringify(record.dynamodb.NewImage.id.S);
+            var roomId = record.dynamodb.NewImage.roomId.N;
+            var flowrate = record.dynamodb.NewImage.flowRate.N;
+            var occupied = record.dynamodb.NewImage.occupied.BOOL;
+
+            var origFlowrate = JSON.stringify(record.dynamodb.OldImage.flowRate.N);
+            var origOccupied = JSON.stringify(record.dynamodb.OldImage.occupied.BOOL);
+
+            var isIssueExisting = isFlowing(flowrate) && !occupied;
+
+            console.log('==========isAlarming: ' + isIssueExisting);
+
+            // read dynamodb Alerts table to find out
+            var docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+
+            var table = "Alert-a6vu6lziizbflprigc7ncn7ta4-hotelmgmt";
+
+            var params = {
+                TableName: table,
+                Key: {
+                    "id": sensorId                }
+            };
+
+            var putObjectPromise = docClient.get(params).promise();
+            putObjectPromise.then(function (data) {
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                var alertAlreadyExists = false;
+                if (data.Item == null) {
+                    console.log("No alarm exists");
+                    alertAlreadyExists = false;
+                } else {
+                    console.log("Alarm exists");
+                    alertAlreadyExists = true;
+                }
+                if (isIssueExisting && !alertAlreadyExists) {
+                    console.log("==========Need to create alarm=====");
+                    // create alarm
+                    createAlarm(sensorId, roomId, ALARM_TYPE_WATERLEAK);
+                } else if (!isIssueExisting && alertAlreadyExists) {
+                    console.log("==========Need to delete alarm=====");
+                    // delete alarm
+                    deleteAlarm(sensorId, roomId, ALARM_TYPE_WATERLEAK);
+                } else if (isIssueExisting && alertAlreadyExists) {
+                    console.log("No action required, since alarm already exists");
+                } else if (!isIssueExisting && !alertAlreadyExists) {
+                    console.log("No action required since alarm does not exist");
+                }
+            }).catch(function (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                console.log(err);
+            });
+
+
+        }
+    });
+    callback(null, `Successfully processed ${event.Records.length} records.`);
+
+
 };
