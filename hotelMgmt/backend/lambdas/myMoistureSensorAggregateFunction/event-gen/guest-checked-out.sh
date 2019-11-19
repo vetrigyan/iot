2ca8dcddf885a43b48fae6406e3d9239bb72dfca
EC2 Default User <ec2-user@ip-172-31-22-181.ec2.internal>
2019-11-19 21:10:25 +0000

    separated the checkin/checkout guest scripts

---
+++ hotelMgmt/backend/lambdas/myMoistureSensorAggregateFunction/event-gen/guest-checked-out.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws sqs send-message --queue-url https://sqs.us-east-1.amazonaws.com/206982232731/myHotelFrontDeskEventsQueue --message-body file://vacant-send-body.json --delay-seconds 10 --message-attributes file://send-message.json
